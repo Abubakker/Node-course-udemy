988656b7add6ae9abb066538481aca4d70017112
Md. Abu bakker siddique <bakker311042@gmail.com>
2019-11-21 12:50:19 +0600

    114. The User/Task Relationship

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('./db/mongoose'); // Data base connection
-//const User = require('./models/user');
-//const Task = require('./models/task');
+const User = require('./models/user');
+const Task = require('./models/task');
 const app = express();
 const port = process.env.PORT || 4001;
 const userRouter = require('./routers/user'); // Routing
@@ -14,3 +14,14 @@ app.use(taskRouter); // Routing
 app.listen(port, () => {
     console.log('Server is up on port ' + port + '.');
 });
+
+const userFind = async () => {
+//    const task = await Task.findById('5dd62481b3d5882af4a67bf2');
+//    await task.populate('owner').execPopulate()
+//    console.log(task)
+
+    const user = await User.findById('5dd3a3d56508fe2dd437c9ab');
+    await user.populate('tasks').execPopulate();
+    console.log(user.tasks);
+};
+userFind();
--- task-manager/src/models/task.js
+++ task-manager/src/models/task.js
@@ -6,9 +6,15 @@ const Task = mongoose.model('Task', {// model, collection name is 'Task'
         type: String,
         required: true,
         trim: true
-    }, completed: {
+    },
+    completed: {
         type: Boolean,
         default: false
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
     }
 });
 
