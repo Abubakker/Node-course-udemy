90b0545cf2a3ed290203bcaa84a43a37ab6f6f33
Md. Abu bakker siddique <bakker311042@gmail.com>
2019-11-22 09:02:50 +0600

    116. Cascade Delete Tasks

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -15,13 +15,13 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port + '.');
 });
 
-const userFind = async () => {
-//    const task = await Task.findById('5dd62481b3d5882af4a67bf2');
-//    await task.populate('owner').execPopulate()
-//    console.log(task)
-
-    const user = await User.findById('5dd3a3d56508fe2dd437c9ab');
-    await user.populate('tasks').execPopulate();
-    console.log(user.tasks);
-};
-userFind();
+//const userFind = async () => {
+////    const task = await Task.findById('5dd62481b3d5882af4a67bf2');
+////    await task.populate('owner').execPopulate()
+////    console.log(task)
+//
+//    const user = await User.findById('5dd3a3d56508fe2dd437c9ab');
+//    await user.populate('tasks').execPopulate();
+//    console.log(user.tasks);
+//};
+//userFind();
