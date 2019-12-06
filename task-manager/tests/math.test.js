efadf0cf7de083775bf01229a402937e9b34b8ca
Md. Abu bakker siddique <bakker311042@gmail.com>
2019-12-06 15:29:05 +0600

    140. Testing Asynchronous Code

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,4 +1,4 @@
-const {calculateTip, fahrenheightToCelsius, celsiusToFahrenheight} = require('../src/math');
+const {calculateTip, fahrenheightToCelsius, celsiusToFahrenheight, add} = require('../src/math');
 
 test('Should calculate total with tip.', () => {
     const total = calculateTip(10, .3);
@@ -9,6 +9,7 @@ test('Should calculate total with tip.', () => {
 //        throw new Error('Total tip should be 13. Got ' + total);
 //    }
 });
+
 test('Should calculate total with defult tip.', () => {
     const total = calculateTip(10);
     expect(total).toBe(12.5);
@@ -18,9 +19,30 @@ test('Should convert 32 F to 0 C', () => {
     const temp = fahrenheightToCelsius(32);
     expect(temp).toBe(0);
 });
+
 test('Should convert 0 C 32 F', () => {
     const temp = celsiusToFahrenheight(0);
     expect(temp).toBe(32);
+});
+
+test('Async test demo', (done) => {
+    setTimeout(() => {
+        expect(1).toBe(1);
+        done();
+    }, 2000);
+});
+
+test('Should add two numbers', (done) => {
+    add(12, 34).then((sum) => {
+        expect(sum).toBe(46);
+        done();
+    })
+});
+
+test('Should add two numbers async/await', async() => {
+    const sum = await add(12, 13).then((sum) => {
+        expect(sum).toBe(25);
+    })
 });
 
 //test('Hello tester!', () => {
