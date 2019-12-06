091bd18c8bbb5f838f782e550858c4b66c6aa167
Md. Abu bakker siddique <bakker311042@gmail.com>
2019-12-06 14:56:18 +0600

    139. Writing Your Own Tests

--- task-manager/src/math.js
+++ task-manager/src/math.js
@@ -5,6 +5,11 @@ const calculateTip = (total, tipPercent = .25) => total + (total * tipPercent); 
 //    return total + tip
 //}
 
+const fahrenheightToCelsius = (temp) => (temp - 32) / 1.8;
+const celsiusToFahrenheight = (temp) => (temp * 1.8) + 32;
+
 module.exports = {
-    calculateTip
-}
+    calculateTip,
+    fahrenheightToCelsius,
+    celsiusToFahrenheight
+};
--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,4 +1,4 @@
-const {calculateTip} = require('../src/math');
+const {calculateTip, fahrenheightToCelsius, celsiusToFahrenheight} = require('../src/math');
 
 test('Should calculate total with tip.', () => {
     const total = calculateTip(10, .3);
@@ -12,6 +12,15 @@ test('Should calculate total with tip.', () => {
 test('Should calculate total with defult tip.', () => {
     const total = calculateTip(10);
     expect(total).toBe(12.5);
+});
+
+test('Should convert 32 F to 0 C', () => {
+    const temp = fahrenheightToCelsius(32);
+    expect(temp).toBe(0);
+});
+test('Should convert 0 C 32 F', () => {
+    const temp = celsiusToFahrenheight(0);
+    expect(temp).toBe(32);
 });
 
 //test('Hello tester!', () => {
