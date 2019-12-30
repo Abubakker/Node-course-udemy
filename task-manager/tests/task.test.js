939594233393f5a71cf4242bdeb1523401bba721
Md. Abu bakker siddique <bakker311042@gmail.com>
2019-12-30 17:36:19 +0600

    149. Testing with Task Data

--- task-manager/tests/task.test.js
+++ task-manager/tests/task.test.js
@@ -4,7 +4,15 @@ const request = require('supertest');
 const app = require('../src/app');
 //const User = require('../src/models/user');
 const Task = require('../src/models/task');
-const {userOneId, userOne, setupDatabase} = require('./fixtures/db.js');
+const {
+    userOneId,
+    userOne,
+    userTwo,
+    taskOne,
+    taskTwo,
+    taskThree,
+    setupDatabase
+} = require('./fixtures/db.js');
 
 beforeEach(setupDatabase);
 
@@ -20,4 +28,33 @@ test('Should create task for user', async() => {
     const task = await Task.findById(response.body._id);
     expect(task).not.toBeNull();
     expect(task.completed).toEqual(false);
+});
+
+test('Should not create task for unauthorization user', async() => {
+    const response = await request(app)
+            .post('/tasks')
+            // .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+            .send({
+                description: 'Form my test'
+            })
+            .expect(401);
+});
+
+test('Should fetch user tasks', async() => {
+    const response = await request(app)
+            .get('/tasks')
+            .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+            .send()
+            .expect(200);
+    expect(response.body.length).toEqual(2);
+});
+
+test('Should delete other user tasks', async() => {
+    const response = await request(app)
+            .delete(`/tasks/${taskOne._id}`)
+            .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+            .send()
+            .expect(404);
+    const task = await Task.findById(taskOne._id);
+    expect(task).not.toBeNull();
 });
