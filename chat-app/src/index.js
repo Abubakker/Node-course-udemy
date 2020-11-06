58d10f4416bcfcf6a29927612cb5465537370d97
Md. Abu bakker siddique <bakker311042@gmail.com>
2020-11-06 10:56:10 +0600

    170. Tracking Users Joining and Leaving

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const socketio = require('socket.io');
 const Filter = require('bad-words');
 const {generateMessage, generateLocation} = require('./utils/messages');
+const {addUser, removeUser, getUser, getUsersInRoom} = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -25,15 +26,21 @@ app.get('/', function (req, res) {
 io.on('connection', (socket) => {
     console.log('New WebSocket connection');
 
-    socket.on('join', ({username, room}) => {
-        socket.join(room);
+    socket.on('join', (option, callback) => {
+        const {error, user} = addUser({id: socket.id, ...option});
+
+        if (error) {
+            return callback(error)
+        }
+
+        socket.join(user.room);
 
         // Message show defult
         socket.emit('message', generateMessage('Welcome!'));
         // Message show when join any user
-        socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined!`));
+        socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined!`));
 
-
+//        callback()
         // socket.emit, io.emit, socket.broadcast.emit
         // io.to.emit, socket.broadcast.to.emit // to use for room
     });
@@ -65,7 +72,12 @@ io.on('connection', (socket) => {
 
     // Message show when disconnect any user
     socket.on('disconnect', () => {
-        io.emit('message', generateMessage('A User has left!'));
+        const user = removeUser(socket.id)
+
+        if (user) {
+            io.to(user.room).emit('message', generateMessage(`${user.username} has left!`));
+        }
+
     });
 
 //    socket.emit('countUpdated', count);
